550f9ac249097cc9c4d22c55a261e08f83cd0f3f
ImMiguelP <anarchxzx@gmail.com>
2024-04-23 12:12:37 -0700

    chroe: init home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { CardForm } from "@/components/CardForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center border"></div>
+    <div className="flex flex-col items-center justify-center min-h-screen text-center border">
+      <CardForm />
+    </div>
   );
 }
