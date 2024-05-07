a1b725d3656e13c6999e9efec1db4b16cd0d505d
ImMiguelP <anarchxzx@gmail.com>
2024-05-06 19:45:25 -0700

    chore: array for forms

---
+++ src/components/FormArrays.tsx
@@ -0,0 +1,8 @@
+export const activityLevels = [
+  { value: "none", label: "None" },
+  { value: "lightly", label: "Lightly Active" },
+  { value: "moderately", label: "Moderately Active" },
+  { value: "extreme", label: "Extremely Active" },
+];
+
+export const GenderOptions = [{ value: "Male" }, { valeu: "Female" }];
