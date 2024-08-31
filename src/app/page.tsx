da55258e798b85349af0c89176deb2f076d49788
ImMiguelP <anarchxzx@gmail.com>
2024-08-31 01:35:45 -0700

    style: format home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center border bg-zinc-900">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center border bg-zinc-900 pt-20">
       <WeightForm />
     </div>
   );
