1700d09f01827174226ffea04b8b17526a123a67
ImMiguelP <anarchxzx@gmail.com>
2024-09-06 06:05:21 -0700

    chore: layout fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,14 +12,19 @@ const navLinks = [
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-zinc-900 bg-opacity-80 text-zinc-300 z-10">
+    <nav className="fixed top-0 left-0 right-0 bg-zinc-900 bg-opacity-80 text-zinc-300 z-[9999]">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
-          <Link href="/" className="text-2xl font-bold text-red-500">GYMIFY</Link>
+          <Link href="/" className="text-2xl font-bold text-red-500">
+            GYMIFY
+          </Link>
           <ul className="flex space-x-6">
             {navLinks.map((link) => (
               <li key={link.href}>
-                <Link href={link.href} className="hover:text-red-500 transition-colors uppercase">
+                <Link
+                  href={link.href}
+                  className="hover:text-red-500 transition-colors uppercase"
+                >
                   {link.label}
                 </Link>
               </li>
